7f8f054df7f1639af715294fb2ed705300b6ec0d
Xavier Robin <xavalias-github@xavier.robin.name>
2015-06-15 23:07:40 +0200

    Roxygen placeholder for internal functions

---
+++ R/Internals.R
@@ -0,0 +1,5 @@
+#' @rdname Internals
+#' @title Internal functions not intended for general use. These functions may be changed or removed at any time without notice.
+#' @name Internals
+NULL
+# Code is in other source code files, this is just a placeholder for roxygen
