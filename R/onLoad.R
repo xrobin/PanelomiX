3bfd81df6f48802935fe713479e927c3dc5fa9df
Xavier Robin <xavalias-github@xavier.robin.name>
2019-05-18 15:55:16 +0200

    Add citation

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,8 @@
 .onLoad <- function(libname, pkgname) {
 	rJava::.jpackage(pkgname, lib.loc = libname)
 }
+
+
+.onAttach <- function(lib, pkg) {
+	packageStartupMessage("Type 'citation(\"PanelomiX\")' for a citation.")
+}
